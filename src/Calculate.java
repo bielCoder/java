9022e944ea88e8cbc3bf3dcd6eeecb3ac3330a5f
Gabriel Henrique Carvalho de Sousa <gdsousa@recordbrasilia.com.br>
2025-04-18 20:03:01 -0300

    Feat:Terminando o operador aritmético

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,12 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Calculate {
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
-        int y = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+        char operadorAritmetico = args[1].charAt(0);
 
-        sum(5,5);
-        minus(5,5);
+        switch (operadorAritmetico) {
+            case '+':
+                sum(x, y);
+                break;
+            case '-':
+                minus(x, y);
+                break;
+            case '*':
+                multiply(x, y);
+                break;
+            case '/':
+                divide(x, y);
+                break;
+            default:
+                System.out.println("Operador inválido: " + operadorAritmetico);
+        }
     }
 
     static void sum(int a, int b) {
@@ -21,5 +34,11 @@ public class Calculate {
         System.out.println(a * b);
     }
 
-    static void divide(int a, int b) {}
+    static void divide(int a, int b) {
+        if (b != 0) {
+            System.out.println(a / b);
+        } else {
+            System.out.println("Erro: divisão por zero!");
+        }
+    }
 }
