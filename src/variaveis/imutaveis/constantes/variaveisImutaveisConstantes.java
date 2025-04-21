6c7946db113e6e998a4677db324ea23258be688d
Gabriel Henrique Carvalho de Sousa <gdsousa@recordbrasilia.com.br>
2025-04-21 17:42:17 -0300

    initial commit

---
+++ src/variaveis/imutaveis/constantes/variaveisImutaveisConstantes.java
@@ -0,0 +1,14 @@
+package variaveis.imutaveis.constantes;
+
+public class variaveisImutaveisConstantes {
+        public static void main(String[] args) {
+            // Varaveis Imutaveis (contantes)
+
+         final double PI = 3.14159;
+
+
+
+
+            System.out.println(PI);
+        }
+}
