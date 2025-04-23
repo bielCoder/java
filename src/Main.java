14f5ce26f1e8c9f30d93dd3e9014e7a14f74c55c
bielCoder <gabrielhenrique9253@gmail.com>
2025-04-23 18:15:52 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import co.music.guitar.Guitar;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Guitar fender = new Guitar("Fender",6);
+        fender.name = "Fender";
+        fender.chords = 6;
+
+
+
+        Guitar ibanez = new Guitar("Ibanez",6);
+        fender.name = "Ibanez";
+        fender.chords = 6;
+
+
+
+
+    }
+}
---
+++ src/co/music/guitar/Guitar.java
@@ -0,0 +1,13 @@
+package co.music.guitar;
+
+public class Guitar {
+    public String name;
+    public int chords;
+
+    public Guitar(String name, Integer chords)
+    {
+           this.name = name;
+           this.chords = chords;
+    }
+
+}
